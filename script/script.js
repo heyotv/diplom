4ea9d584aa6c74a7756db9a635944f94666c462c
Vitali Tumilovich <hey.tumilovich@gmail.com>
2019-08-08 23:17:45 +0300

    finish

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+
     $(document).scroll(function () {
         var $head = $("#header");
         var $nav = $("#navbar");
@@ -8,17 +9,30 @@ $(document).ready(function () {
         );
     });
 
+
+    $("#menu").on('click', 'a', function () {
+        var id = $(this).attr('href'),
+        top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 1500);
+    });
+
+
     $('.carousel').carousel({
         interval: 1500
     });
 
+
     $('[data-toggle="popover"]').popover();
+
 
     $('.icon-link').on('click', function() {
         $(this).children().toggleClass('icon-blue');
     });
 
+
     $('.icon-heart').on('click', function() {
         $(this).children().toggleClass('icon-red');
     });
+
+    
 });
